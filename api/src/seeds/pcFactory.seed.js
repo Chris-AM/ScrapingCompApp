55831ac5e2432dba5b150e88d2e00df328668f69
chris-am <c.aranguizm@outlook.com>
2022-04-23 15:21:36 -0400

    getting pc factory notebooks links

--- api/src/seeds/pcFactory.seed.js
+++ api/src/seeds/pcFactory.seed.js
@@ -3,14 +3,28 @@ const axios = require('axios');
 const {PCFACTORY} = require('../endpoints/pcFactory');
 
 
-const extractCategories = async () => {
+const extractNotebooks = async () => {
 
     const url = PCFACTORY.BASE_URL + PCFACTORY.NOTE_BOOKS;
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    const categories = [];
-    console.log('extractCategories' + $);
-
+    const {data} = await axios.get(url);
+    const $ = cheerio.load(data);
+    const notebooks = $('div.product-list');
+    //console.log('notebooks ===> ', notebooks);
+    const notebooksArray = ["name"];
+    for(let i = 0 ; i < notebooks.length; i++){
+        const notebook = notebooks[i];
+        const notebooksDivs = $(notebook).find('div.product');
+        for(let j = 0; j < notebooksDivs.length; j++){
+            const notebookDiv = notebooksDivs[j];
+            //console.log('notebookDiv ===>', notebookDiv);
+            const productRelative = $(notebookDiv).find('div.p-relative');
+            //console.log('productRelative ===>', productRelative);
+            const productPath = $(productRelative).find('a.product-ab-link').attr('href');
+            //console.log('product path ===>', productPath);
+        }
+        
+    }
+    return notebooksArray;
 }
 
-module.exports = {extractCategories};
+module.exports = {extractCategories: extractNotebooks};
