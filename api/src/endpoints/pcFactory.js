3495fd7fa5b636f611db1e74b2dd764e49d30310
chris-am <c.aranguizm@outlook.com>
2022-04-23 18:33:40 -0400

    pcfactory's profucts endpoint added

--- api/src/endpoints/pcFactory.js
+++ api/src/endpoints/pcFactory.js
@@ -1,6 +1,7 @@
 const PCFACTORY = {
     "BASE_URL": "https://www.pcfactory.cl/",
-    "NOTE_BOOKS": "notebooks?categoria=735&papa=636"
+    "NOTE_BOOKS": "notebooks?categoria=735&papa=636",
+    "PRODUCT": "producto/"
 }
 
 module.exports = {PCFACTORY};
