8da943ed9c018be835f4ee09acb5364b79f338b6
chris-am <c.aranguizm@outlook.com>
2022-04-26 16:12:17 -0400

    adding pc factory store id

--- api/src/endpoints/pcFactory.js
+++ api/src/endpoints/pcFactory.js
@@ -1,7 +1,8 @@
 const PCFACTORY = {
     "BASE_URL": "https://www.pcfactory.cl/",
     "NOTE_BOOKS": "notebooks?categoria=735&papa=636",
-    "PRODUCT": "producto/"
+    "PRODUCT": "producto/",
+    "STORE_ID": "6267809bef02b977d1eeedba"
 }
 
 module.exports = {PCFACTORY};
