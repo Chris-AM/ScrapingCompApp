91dc235f7f76ddf920e0d53b78d64d9d733d9caf
chris-am <c.aranguizm@outlook.com>
2022-04-26 16:14:06 -0400

    adding store id variable

--- api/src/seeds/pcFactory.seed.js
+++ api/src/seeds/pcFactory.seed.js
@@ -33,23 +33,32 @@ const getNotebooksInfo = async (notebookName = '') => {
     const url = PCFACTORY.BASE_URL + PCFACTORY.PRODUCT + notebookName;
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
+    const storeId = PCFACTORY.STORE_ID;
     //obtainint id from url
-    const notebookId = notebookName.substring(0, notebookName.indexOf('-'));
+    const notebookId = Number.parseInt(notebookName.substring(0, notebookName.indexOf('-')));
     //removing PcFactory's products wrapper
     let productNameWrapper = $('div.product-single__description-mobile');
     let productImageWraper = $('div.product-single__image-container');
     let productImageUrlWrapper = $(productImageWraper).find('div.product-single__image');
     let productSummaryWrapper = $('div[data-tab="fichatecnica"]');
     let productSummaryTableWrapper = $(productSummaryWrapper).find('div.table');
+    let productPriceWrapper = $('div.product-single__price');
     //getting PcFactory's products Info
     let productName = $(productNameWrapper).find('div.paragraph, color-dark-2').text();
     let productImage = PCFACTORY.BASE_URL + $(productImageUrlWrapper).find('img').attr('src');
     let productSummary = $(productSummaryTableWrapper).find('div.table__content, table__content--two-column').text();
-    if( !notebookId || !productName || !productImage || !productSummary){
+    let productPrice = Number.parseInt($(productPriceWrapper).find('meta[itemprop="price"]').attr('content'));
+    if (!notebookId || !productName || !productImage || !productSummary) {
         return null;
     }
     const productInfo = {
-        notebookId, url, productName, productImage, productSummary
+        storeId,
+        notebookId,
+        url,
+        productName, 
+        productImage, 
+        productSummary, 
+        productPrice
     }
     //console.log('productInfo ===>', productInfo);
     return productInfo;
