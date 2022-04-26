1ad95a92d885a932d6baffe4ed5f5459f189764c
chris-am <c.aranguizm@outlook.com>
2022-04-26 08:05:43 -0400

    deleting error 400

--- api/src/controllers/stores.controller.js
+++ api/src/controllers/stores.controller.js
@@ -3,27 +3,14 @@ const { request, response } = require('express');
 const Store = require('../models/stores.model');
 
 const createStore = async (req = request, res = response) => {
-
     const store = new Store({ ...req.body});
-    
     try {
         const newStore = await store.save();
-        
-        
         res.status(200).json({
             ok: true,
             message: 'Store created successfully',
             newStore
         });
-        if(res.status === 400){
-            console.log('store ===> ', store);
-            console.log('req.body ===> ', req.body);
-        console.log('newStore ===> ', newStore);
-            res.status(400).json({
-                ok: false,
-                message: 'Bad request',
-            });
-        }
     } catch (error) {
         console.log('error ===> ', error);
         res.status(500).json({
