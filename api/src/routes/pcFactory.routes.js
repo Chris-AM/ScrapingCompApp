dd48f27e6ba707ed20773929620335fb283aa1a7
chris-am <c.aranguizm@outlook.com>
2022-04-26 16:13:28 -0400

    adding validator middleware to pc factory router

--- api/src/routes/pcFactory.routes.js
+++ api/src/routes/pcFactory.routes.js
@@ -1,10 +1,20 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { loadNotebooks, getNotebooks } = require('../controllers/pcFactory.controller');
 
-const { Router } = require('express');
 
 const router = Router();
 
-router.post('/', loadNotebooks);
+router.post('/:id', [
+    check('id', 'product id is required').not().isEmpty(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('url', 'Url is required').not().isEmpty(),
+    check('image', 'Image is required').not().isEmpty(),
+    check('price', 'Price is required').not().isEmpty(),
+    check('summary', 'Summary is required').not().isEmpty(),
+    check('store', 'Store is required').isMongoId(),
+], loadNotebooks);
 router.get('/', getNotebooks);
 
 module.exports = router;
